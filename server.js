71e37a457cbed0fbf94f9476c24052f3339efbe1
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-01-15 14:55:58 +0700

    add route product add

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-'use strict';
+"use strict";
 
-const Hapi = require('hapi');
+const Hapi = require("hapi");
 
 const dbOpts = {
-    url: 'mongodb://admin:admin@cluster0-shard-00-00-sdzlc.gcp.mongodb.net:27017,cluster0-shard-00-01-sdzlc.gcp.mongodb.net:27017,cluster0-shard-00-02-sdzlc.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    url:
+        "mongodb://admin:admin@cluster0-shard-00-00-sdzlc.gcp.mongodb.net:27017,cluster0-shard-00-01-sdzlc.gcp.mongodb.net:27017,cluster0-shard-00-02-sdzlc.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
     settings: {
         poolSize: 10
     },
@@ -11,53 +12,64 @@ const dbOpts = {
 };
 // Create a server with a host and port
 const server = Hapi.server({
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: process.env.PORT || 8000
 });
 
-
-server.route( {
-    method: 'GET',
-    path: '/users/{id}',
+server.route({
+    method: "GET",
+    path: "/users/{id}",
     async handler(request) {
-
         const db = request.mongo.db;
         const ObjectID = request.mongo.ObjectID;
 
         try {
-            const result = await db.collection('account').findOne({  _id: new ObjectID(request.params.id) });
+            const result = await db
+                .collection("account")
+                .findOne({ _id: new ObjectID(request.params.id) });
             return result;
-        }
-        catch (err) {
-            throw Boom.internal('Internal MongoDB error', err);
+        } catch (err) {
+            throw Boom.internal("Internal MongoDB error", err);
         }
     }
 });
 // Add the route List Product
 server.route({
-    method: 'GET',
-    path: '/product/list',
-    handler: () => { 
+    method: "GET",
+    path: "/product/list",
+    handler: () => {
         return server.methods.product.ListProduct();
     }
-})
+});
+
+server.route({
+    method: "POST",
+    path: "/product/add",
+    handler: (request, reply) => {
+        return server.methods.product
+            .AddProduct(server, request)
+            .then(reply);
+    }
+});
 
 // Start the server
 const start = async function () {
-
     try {
-        await server.register([require('./hapi-my-water-product'),{
-            plugin: require('hapi-mongodb'),
-            options: dbOpts
-        }]);
+        await server.register([
+            {
+                plugin: require("hapi-mongodb"),
+                options: dbOpts
+            },
+            require("./hapi-my-water-product"),
+            require("./hapi-my-water-datasource")
+        ]);
         await server.start();
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
         process.exit(1);
     }
 
-    console.log('Server running at:', server.info.uri);
+    console.log("Server running at:", server.info.uri);
 };
 
 start();

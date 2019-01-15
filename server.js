725fef2f31cd581d4ec98039857074d27e6a10e3
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-01-15 10:29:00 +0700

    add get product list

--- server.js
+++ server.js
@@ -17,6 +17,35 @@ server.route({
     }
 });
 
+// Add the route List Product
+server.route({
+    method: 'GET',
+    path: '/product/list',
+    handler: () => { 
+        return {
+            "status": 200,
+            "message": "success",
+            "data": [
+                {
+                    "id": 1,
+                    "name": "Nam 1",
+                    "imageUrl": "water.jpg",
+                    "price": 99.99,
+                    "expire": "2018-12-31"
+                },
+                {
+                    "id": 1,
+                    "name": "Nam 1",
+                    "imageUrl": "water.jpg",
+                    "price": 99.99,
+                    "expire": "2018-12-31"
+                }
+            ]
+        };
+    }
+})
+
+
 // Start the server
 const start = async function () {
 

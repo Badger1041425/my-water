4e24dbc05d0bf50359b0707facdcd0b8a0e866ac
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-01-15 14:55:43 +0700

    add product add

--- hapi-my-water-product.js
+++ hapi-my-water-product.js
@@ -5,14 +5,55 @@ exports.plugin = {
 
         server.method({
             name: "product.ListProduct",
-            method: getProductList,
-            
+            method: getProductList
+        });
 
-        })
+        server.method({
+            name: "product.AddProduct",
+            method: addProduct
+        });
 
     }
 };
-var getProductList=() => {
+
+const addProduct = (server, request) => {
+    const body = {
+        name: request.payload.name,
+        imageUrl: request.payload.imageUrl,
+        price: request.payload.price,
+        expire: request.payload.expire
+    }
+    return new Promise((resolve, reject) => {
+        server.methods.datasource.product.Insert(request.mongo.db, body)
+            .then((res) => {
+                if (res.result.ok == 1) {
+                    console.log(res.ops)
+                    resolve({
+                        status: 200,
+                        message: "เพิ่มได้เว้ยยยยยยย",
+                        data: (res.ops && res.ops.length > 0) ? res.ops[0] : {}
+                    });
+                } else {
+                    reject({
+                        status: 500,
+                        message: "เพิ่มไม่ได้เว้ยยยยยยย",
+                        data: null
+                    });
+                }
+            }).catch((error) => {
+                console.log(error);
+                reject({
+                    status: 500,
+                    message: "เพิ่มไม่ได้เว้ยยยยยยย",
+                    data: null
+                });
+            });
+    });
+
+}
+
+
+var getProductList = () => {
     return {
         "status": 200,
         "message": "success",

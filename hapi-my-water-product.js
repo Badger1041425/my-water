6cdba28ba72f1cbe098b983004b68d5e3219d658
oat <oat19454@gmail.com>
2019-01-15 17:18:59 +0700

    delete old getProductList

--- hapi-my-water-product.js
+++ hapi-my-water-product.js
@@ -96,26 +96,3 @@ var removeProduct = (server, request) =>{
         });
     });
 }
-
-var getProductList = () => {
-    return {
-        "status": 200,
-        "message": "success",
-        "data": [
-            {
-                "id": 1,
-                "name": "Nam 1",
-                "imageUrl": "water.jpg",
-                "price": 99.99,
-                "expire": "2018-12-31"
-            },
-            {
-                "id": 1,
-                "name": "Nam 1",
-                "imageUrl": "water.jpg",
-                "price": 99.99,
-                "expire": "2018-12-31"
-            }
-        ]
-    };
-}
